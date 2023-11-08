31a425fb9631e73080c500d3320ddc116d4a300b
Shiva919199 <134729190+Shiva919199@users.noreply.github.com>
2023-11-08 23:06:58 +0530

    Add files via upload

---
+++ B/student_main.sol
@@ -0,0 +1,43 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0;
+
+contract Student_management
+{
+    struct Student
+    {
+        int stud_id;
+        string Name;
+        string Department;
+    }
+
+    Student[] Students;
+
+    function add_student( int stud_id, string memory Name, string memory Department) public 
+    {
+        Student  memory stud = Student(stud_id, Name, Department);
+        Students.push(stud);
+    }
+
+    function get_student(int stud_id) public view returns(string memory, string memory)
+    {
+        for (uint i = 0; i < Students.length ; i++)
+        {
+            Student memory stud = Students[i];
+            if (stud.stud_id == stud_id)
+            {
+                return (stud.Name, stud.Department);
+            }
+        }
+        return ("Name Not Found", "Department Not Found"); 
+    } 
+
+    function get_student_count() public view returns(uint256)
+    {
+        return Students.length;
+    }
+
+    fallback() external 
+    {
+        add_student(2,"Unknown","CSE");
+    }
+}
